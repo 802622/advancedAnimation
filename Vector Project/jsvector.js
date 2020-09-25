ad1e587b14023bee34e898882fd6a46df20e4def
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2020-09-25 10:56:48 -0700

    Update jsvector.js

--- Vector Project/jsvector.js
+++ Vector Project/jsvector.js
@@ -10,18 +10,22 @@ function JSVector(x = 0,y = 0){
 // Set the magnitude of the vector,
 // retaining the angle (direction).
 JSVector.prototype.setMagnitude = function(mag){
-
+	var direction = this.getDirection(); 
+	this.x = Math.cos(direction) * mag
+	this.y = Math.sin(direction) * mag
 }
 
 // Get the magnitude of the vector using pythagorean theorem
 JSVector.prototype.getMagnitude = function(){
-
+	return Math.sqrt(this.x * this.x + this.y * this.y);
 }
 
 // Set the angle (direction) of the vector,
 // retaining the magnitude.
 JSVector.prototype.setDirection = function(angle){
-
+	var magnitude = this.getMagnitude();
+  this.x = Math.cos(angle) * magnitude;
+  this.y = Math.sin(angle) * magnitude;
 }
 
 // Get the direction (angle) of the vector
@@ -31,7 +35,8 @@ JSVector.prototype.getDirection = function(){
 
 // Add another vector to this vector
 JSVector.prototype.add = function(v2){
-
+	this.x += v2.x;
+  this.y += v2.y;
 }
 
 // Subtract another vector from this vector
@@ -41,22 +46,23 @@ JSVector.prototype.sub = function(v2){
 
 // Class method to return a new vector that is the sum of two vectors
 JSVector.addGetNew = function(v1,v2){
-
+	return new Vector(this.x + v2.x, this.y + v2.y);
 }
 
 // Class method to return a new vector that is the difference of two vectors
 JSVector.subGetNew = function(v1,v2){
-
+	return new Vector(this.x - v2.x, this.y - v2.y);
 }
 
 // Multiply this vector by a scalar
 JSVector.prototype.multiply = function(scalar){
-
+  this.x *= scalar;
+  this.y *= scalar;
 }
 
 // Divide this vector by a scalar
 JSVector.prototype.divide = function(scalar){
-
+  return new Vector(this.x / scalar, this.y / scalar);
 }
 
 // Normalize this vector so that it has a magnitude of 1
