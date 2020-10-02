2e8ea0a7d6d6b5ea1b22f1e614b19214c52e0afc
NikolaTesla17 <44534701+NikolaTesla17@users.noreply.github.com>
2020-10-02 09:46:34 -0700

    vectors mostly work

--- 2021 DOM Start Template/bubble.js
+++ 2021 DOM Start Template/bubble.js
@@ -1,18 +1,17 @@
 //  Bubble constructor function +++++++++++++++++++++++++++++
-function Bubble(x, y, dx, dy, rad, clr){
-    this.x = x;
-    this.y = y;
-    this.dx = dx;
-    this.dy = dy;
-    this.rad = rad;///2;
-    this.clr = clr;
-    this.isOverlapping = false;
-}
 
+function Bubble(x, y, dx, dy, rad, clr){
+  this.location = new JSVector(x, y);
+  this.velocity = new JSVector(dx, dy);
+
+  this.rad = rad;
+  this.clr = clr;
+  this.isOverlapping = false;
+}
   //  placing methods in the prototype (every bubble shares functions)
 Bubble.prototype.run = function(){
     this.checkEdges();
-    this.checkOverlapping()
+    this.checkOverlapping();
     this.update();
     this.render();
   }
@@ -20,14 +19,14 @@ Bubble.prototype.run = function(){
 // check if this bubble is overlapping any other bubble
 Bubble.prototype.checkOverlapping = function(){
     this.isOverlapping = false;//  default color
-    this.clr =  "rgba(255,255,255,255)"
+    this.clr =  "rgba(255,255,255,255)";
     let b = game.bubbles;
     for(let i = 0; i < b.length; i++){ // for all the bubbles
        if(this !== b[i]){   // if not this bubble
-         let d = Math.sqrt((this.x-b[i].x)*(this.x-b[i].x) + (this.y-b[i].y)*(this.y-b[i].y));
+         var d = this.location.distance(b[i].location);
          if(d < this.rad + b[i].rad){
             this.isOverlapping = true;
-            this.clr =  "rgba(100,220,55,10)"
+            this.clr =  "rgba(100,220,55,10)";
          }
        }
     }
@@ -39,16 +38,16 @@ Bubble.prototype.render = function(){
     let ctx = game.ctx;
     // color depends on whether this bubble overlaps any oher bubble
     if(this.isOverlapping){
-        ctx.strokeStyle = "rgba(255,255,255,255)"//this.clr;
+        ctx.strokeStyle = "rgba(255,255,255,255)";//this.clr;
         ctx.fillStyle = this.clr;
         ctx.beginPath();
-        ctx.arc(this.x,this.y, this.rad, Math.PI*2, 0, false);
+        ctx.arc(this.location.x,this.location.y, this.rad, Math.PI*2, 0, false);
         ctx.stroke();
         ctx.fill();
     }else{
         ctx.strokeStyle = this.clr;
         ctx.beginPath();
-        ctx.arc(this.x,this.y, this.rad, Math.PI*2, 0, false);
+        ctx.arc(this.location.x,this.location.y, this.rad, Math.PI*2, 0, false);
         ctx.stroke();
     }
 
@@ -57,18 +56,26 @@ Bubble.prototype.render = function(){
 // Move the bubble in a random direction
 Bubble.prototype.update = function(){
     if(!game.gamePaused){
-      this.dx = Math.random()*6-3;
-      this.dy = Math.random()*6-3;
-      this.x += this.dx;
-      this.y += this.dy;
+      this.velocity.dx = Math.random()*6-3;
+      this.velocity.dy = Math.random()*6-3;
+      this.location.add(this.velocity);
     }
   }
 
 // When a bubble hits an edge of the canvas, it wraps around to the opposite edge.
 Bubble.prototype.checkEdges = function(){
     let canvas = game.canvas;
-    if(this.x > canvas.width)  this.x = 0; // wrap around from right to left
-    if(this.x < 0)  this.x = canvas.width; // wrap around from left to right
-    if(this.y > canvas.height)  this.y = 0; // wrap around from bottom to top
-    if(this.y < 0)  this.y = canvas.height; // wrap around from top to bottom
+    if (this.location.x > canvas.width){
+      this.location.x = 0;
+    }
+    else if(this.location.x < 0){
+      this.location.x = canvas.width;
+    }
+    if (this.location.y > canvas.height){
+      this.location.y = 0;
+    }
+    else if(this.location.y < 0){
+      this.location.y = canvas.height;
+    }
+
   }
