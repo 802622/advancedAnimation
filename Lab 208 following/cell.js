ac8a2a2ca6074a1aa986cc73c5d61086252a6fb1
Nayan Smuek <44534701+NikolaTesla17@users.noreply.github.com>
2021-02-12 10:12:51 -0800

    Update cell.js

--- Lab 208 following/cell.js
+++ Lab 208 following/cell.js
@@ -23,17 +23,12 @@ class Cell {
             ctx.fillRect(this.loc.x, this.loc.y,this.width, this.height);
         }
         //render cell
-        // ctx.fillStyle = this.clr;
         ctx.strokeStyle = "black";
         ctx.lineWidth = 1;
         ctx.beginPath();
         ctx.rect(this.loc.x, this.loc.y, this.width, this.height);
         // ctx.fill();
         ctx.stroke();
-        // ctx.font = '10px serif';
-        // ctx.strokeText("r = " + this.r, this.loc.x + 5, this.loc.y + 11);
-        // ctx.strokeText("c = " + this.c, this.loc.x + 5, this.loc.y + 31);
-
     }
 
     update() {
